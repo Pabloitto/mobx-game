426b86fabde534fb2af74586aaa0fe0f7c182812
Pablo Itto <pablo.itto@unosquare.com>
2018-04-08 14:20:23 -0700

    Structure

---
+++ client/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+import App from './components/app'
+import 'bootstrap-css-only/css/bootstrap.css'
+
+ReactDOM.render(
+  (
+    <Router history={hashHistory}>
+      <Route path='/' component={App} />
+    </Router>
+  ),
+  document.getElementById('root')
+)
