c89dabf7e8cb3b933f85f35cbac69d5a90aea9f6
baldric <baldric@riberion.io>
2018-08-17 14:37:44 -0500

    Updated packages and Wepack file to lastest version

--- client/index.js
+++ client/index.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
 import App from './components/app'
-import Home from './components/home'
-import Albums from './components/albums'
-import Posts from './components/posts'
 import 'bootstrap-css-only/css/bootstrap.css'
 import './content/css/dashboard.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  (
-    <Router history={hashHistory}>
-      <Route component={App}>
-        <Route path='/home' component={Home} />
-        <Route path='/albums' component={Albums} />
-        <Route path='/posts' component={Posts} />
-      </Route>
-      <Redirect from='*' to='/home' />
-    </Router>
-  ),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
-)
+);
